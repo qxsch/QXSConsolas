bb50cd5937c845bc7cd42f5532b02cae2bf23c90
qxsch <qxsch@users.noreply.github.com>
2016-03-01 12:20:56 +0100

    various improvements
        added SplunkRole APIs
        added Url parser
        added indexes.conf check

--- examples/CopyThat/copyThat/CopyThat/SplunkRoles.py
+++ examples/CopyThat/copyThat/CopyThat/SplunkRoles.py
@@ -5,12 +5,12 @@ from QXSConsolas.Command import SSH, call
 from clint.textui import puts, colored, indent
 import timeit
 import abc
-
+from urlparse import urlparse
 
 class SplunkRole(object):
     __metaclass__ = abc.ABCMeta
 
-    _app = None
+    app = None
     _appname = None
     _appdir = None
     _appconfig = None
@@ -39,7 +39,7 @@ class SplunkRole(object):
         roleconfig QXSConsolas.Configuration.Configuration
                    configuration of the role, where the app shoukd be deplyed
 	"""
-        self._app = cliapp
+        self.app = cliapp
         self._appname = appname
         self._appdir = appdir
         self._appconfig = appconfig
@@ -61,6 +61,18 @@ class SplunkRole(object):
 	"""
         pass
 
+    def _syncLocalAppToRemote(self, ssh, remoteappdir, srvconfig):
+        # use rsync
+        pass
+
+    def _removeFileFromApp(self, ssh, remoteappdir, filename):
+        rc, stdout, stderr = ssh.call(["find", remoteappdir, "-name", filename, "-print", "-delete" ])
+        if rc == 0:
+            if stdout.strip() != "":
+                self.app.logger.warning("Deleted the following files on server \"" + ssh.host + "\":\n" + (stdout.strip() + "\n" + stderr.strip()).strip())
+        else:
+            self.app.logger.error("Failed to delete file \"" + filename + "\" from app on server \"" + ssh.host + "\" with rc " + rc + ":\n" + (stdout.strip() + "\n" + stderr.strip()).strip())
+
 
 class SearchHeadRole(SplunkRole):
     def deployAppToServer(self, ssh, remoteappdir, srvconfig):
@@ -73,9 +85,12 @@ class SearchHeadRole(SplunkRole):
         srvconfig    QXSConsolas.Configuration.Configuration
                      server configuration with hostname and path
 	"""
-        #print(self._envconfig.dump())
-        print(srvconfig["hostname"] + " " + srvconfig["path"])
-
+        self._syncLocalAppToRemote(ssh, remoteappdir, srvconfig)
+        self._removeFileFromApp(ssh, remoteappdir, "indexes.conf")
+        url, user, password = splitUrlCreds(srvconfig)
+        print(splitUrlCreds(srvconfig))
+        #rc, stdout, stderr = ssh.call([])
+        #splunk apply shcluster-bundle --answer-yes -target https://hostname:port -auth user:pw
 
 class IndexerRole(SplunkRole):
     def deployAppToServer(self, ssh, remoteappdir, srvconfig):
@@ -88,8 +103,11 @@ class IndexerRole(SplunkRole):
         srvconfig    QXSConsolas.Configuration.Configuration
                      server configuration with hostname and path
 	"""
-        #print(self._envconfig.dump())
-        print(srvconfig["hostname"] + " " + srvconfig["path"])
+        self._syncLocalAppToRemote(ssh, remoteappdir, srvconfig)
+        url, user, password = splitUrlCreds(srvconfig)
+        print(splitUrlCreds(srvconfig))
+        #rc, stdout, stderr = ssh.call([])
+        #splunk apply cluster-bundle --answer-yes -target https://hostname:port -auth user:pw
 
 
 class UnifiedForwarderManagementRole(SplunkRole):
@@ -103,9 +121,40 @@ class UnifiedForwarderManagementRole(SplunkRole):
         srvconfig    QXSConsolas.Configuration.Configuration
                      server configuration with hostname and path
 	"""
-        #print(self._envconfig.dump())
-        print(srvconfig["hostname"] + " " + srvconfig["path"])
+        self._syncLocalAppToRemote(ssh, remoteappdir, srvconfig)
+        self._removeFileFromApp(ssh, remoteappdir, "indexes.conf")
+        url, user, password = splitUrlCreds(srvconfig)
+        print(splitUrlCreds(srvconfig))
+        #rc, stdout, stderr = ssh.call([])
+        #splunk reload deploy-server --answer-yes -target https://hostname:port -auth user:pw
 
+
+def splitUrlCreds(url):
+    if type(url) == str:
+        target = urlparse(url)
+    elif "target" in url:
+        target = urlparse(url["target"])
+    elif "url" in url:
+        target = urlparse(url["url"])
+    else:
+        return [ None, None, None ]
+    url = ""
+    if target.scheme == "":
+        url += "http://"
+    else:
+        url += target.scheme + "://"
+    if target.hostname is None or target.hostname == "":
+        url += "localhost"
+    else:
+        url += target.hostname
+    if not target.port is None:
+        url += ":" + str(target.port)
+    if target.path[0] != "/":
+        url += "/"
+    url += target.path
+    if target.params != "":
+        url += "?" + target.params
+    return [ url, target.username, target.password ]
 
 def getSplunkRoles():
     roles = {}
