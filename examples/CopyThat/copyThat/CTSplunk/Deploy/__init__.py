c1cc86b26f8856cb5fe119e5fde9f5dedc42c5c3
qxsch <qxsch@users.noreply.github.com>
2016-06-06 16:44:21 +0200

    added available roles and envs in the help

--- examples/CopyThat/copyThat/CTSplunk/Deploy/__init__.py
+++ examples/CopyThat/copyThat/CTSplunk/Deploy/__init__.py
@@ -3,13 +3,34 @@ from QXSConsolas.Cli import CliApp
 from clint.textui import puts, colored, indent
 from CTSplunk import NoRolesToDeployException, DeploymentException, AppNotFoundException
 from SplunkDeployer import SplunkDeployer
+from QXSConsolas.Configuration import GetSystemConfiguration
+
+
+def GetConfiguredEnvs(sep=", "):
+    l = [ "ALL" ]
+    envs = GetSystemConfiguration().get("SplunkDeployment.envs")
+    for env in envs:
+        l.append(env)
+    return str(sep).join(l)
+
+def GetConfiguredRoles(sep=", "):
+    l = []
+    envs = GetSystemConfiguration().get("SplunkDeployment.envs")
+    for env in envs:
+        for role in envs[env]:
+            if role not in l:
+                l.append(role)
+    return str(sep).join(l)
+
+
+
 
 @CliApp(
     Name = "Deploy an app",
     Description = "Deploys an app to a splunk environment",
     Opts = [ 
         { "argument": "--app:", "required": True, "multiple": True,  "description": "The app that should be deployed", "valuename": "APP" },
-        { "argument": "--env:", "default": "ALL", "description": "The targeted Splunk environment", "valuename": "ENV" },
+        { "argument": "--env:", "default": "ALL", "description": "The targeted Splunk environment.\nAvailable environments:\n * " + GetConfiguredEnvs("\n * "), "valuename": "ENV" },
     ]
 )
 def DeployApp(app):
@@ -27,8 +48,8 @@ def DeployApp(app):
     Description = "Creates an app on a splunk environment",
     Opts = [ 
         { "argument": "--app:",  "required": True, "multiple": True,  "description": "The app that should be deployed", "valuename": "APP" },
-        { "argument": "--role:", "required": True, "multiple": True,  "description": "Create the app on the following roles", "valuename": "ROLE" },
-        { "argument": "--env:", "default": "ALL", "description": "The targeted Splunk environment", "valuename": "ENV" },
+        { "argument": "--role:", "required": True, "multiple": True,  "description": "Create the app on the following roles.\nAvailable roles:\n * " + GetConfiguredRoles("\n * "), "valuename": "ROLE" },
+        { "argument": "--env:", "default": "ALL", "description": "The targeted Splunk environment.\nAvailable environments:\n * " + GetConfiguredEnvs("\n * "), "valuename": "ENV" },
         { "argument": "-f", "description": "Force the app creation." },
     ]
 )
@@ -47,7 +68,7 @@ def CreateApp(app):
     Description = "Removes an app from a splunk environment",
     Opts = [ 
         { "argument": "--app:",  "required": True, "multiple": True,  "description": "The app that should be deployed", "valuename": "APP" },
-        { "argument": "--env:", "default": "ALL", "description": "The targeted Splunk environment", "valuename": "ENV" },
+        { "argument": "--env:", "default": "ALL", "description": "The targeted Splunk environment.\nAvailable environments:\n * " + GetConfiguredEnvs("\n * "), "valuename": "ENV" },
     ]
 )
 def RemoveApp(app):
